8185b5d0835949b3169315708526470a1a839699
attoyibi <52569071+muhammad-muchson@users.noreply.github.com>
2023-09-26 12:46:39 +0700

    update join page
    merubah warna, menghilangkan button sign in, akun, dan menambahkan halaman

--- chat-ai/src/app/join/page copy.js
+++ chat-ai/src/app/join/page copy.js
@@ -1,7 +1,7 @@
 // `app/dashboard/page.js` is the UI for the `/dashboard` URL
 export default function Page() {
   return (
-    <div className="bg-white relative max-w-[500px] mx-auto w-full flex flex-col min-h-screen shadow-xl shadow-primary/40">
+    <div className="relative max-w-[500px] mx-auto w-full flex flex-col min-h-screen shadow-xl shadow-primary/40">
       <div>
         <div className="fixed w-full z-10">
           <div className="grid grid-cols-9 gap-3 px-5 py-3 max-w-[500px] bg-white">
@@ -49,40 +49,60 @@ export default function Page() {
       <div className="px-6 animate-[from-r-25_.35s_ease-in-out]">
         <div className="mt-5">
           <p className="font-bold text-neutral text-xl lg:text-center">
-            Selamat datang! Daftarkan Email anda untuk gabung ke Avatara.
+            Selamat datang! Daftarkan nomor HP anda untuk gabung.
           </p>
           <div className="mt-5">
-            {/* <button className="tab tab-bordered tab-active pointer-events-none">
-              Nomor HP
-            </button> */}
-            {/* <button className="tab">Nomor Hp</button> */}
+            <button className="tab">Email</button>
+            <button className="tab tab-bordered tab-active pointer-events-none">
+              Nomor Hp
+            </button>
             <div className="mt-5">
-              <div className="animate-[from-l-25_.35s_ease-in-out]">
-                <p>Silahkan daftar menggunakan Nomor HP anda.</p>
-                <div className="relative mt-5">
-                  <div className="flex justify-between items-end mb-1">
-                    <label className="block text-zinc-500 font-bold text-sm">
-                      Nomor HP
-                    </label>
+              <div className="animate-[from-r-25_.35s_ease-in-out]">
+                <p>Silahkan daftar menggunakan nomor HP anda.</p>
+                <div className="grid grid-cols-6 font-semibold w-full items-end mt-5">
+                  <select
+                    disabled=""
+                    className="select select-bordered border-1 border-solid border-[#E3E3E3] mr-2 col-span-2 mb-4 cursor-not-allowed"
+                  >
+                    <option disabled="" value="DEFAULT">
+                      Pick your choice
+                    </option>
+                    <option value="62">+62</option>
+                  </select>
+                  <div className="relative w-full col-span-4">
+                    <div className="flex justify-between items-end mb-1">
+                      <label className="block text-zinc-500 font-bold text-sm">
+                        No. HP
+                      </label>
+                    </div>
+                    <div className="flex items-center w-full border rounded-4 p-3 text-zinc-700 rounded-lg border-zinc-200">
+                      <input
+                        role="textbox-tel"
+                        className="w-full bg-transparent text-base-content outline-none"
+                        placeholder="123456789"
+                        type="tel"
+                        value=""
+                      />
+                    </div>
+                    <div className="text-[10px] font-medium h-4 text-zinc-400"></div>
                   </div>
-                  <div className="flex items-center w-full border rounded-4 p-3 text-zinc-700 rounded-lg border-zinc-200">
-                    <input
-                      role="textbox-text"
-                      className="w-full bg-transparent text-base-content outline-none"
-                      placeholder="+62xxxxxxxxxx"
-                      type="text"
-                      value=""
-                    />
-                  </div>
-                  <div className="text-[10px] font-medium h-4 text-zinc-400"></div>
                 </div>
-                <button
-                  role="button"
-                  disabled=""
-                  className="btn btn-primary text-primary-content disabled:cursor-not-allowed disabled:opacity-60 text-center relative w-full rounded-full mt-5"
-                >
-                  Kirim OTP
-                </button>
+                <div className="grid grid-cols-2 gap-3 mt-5">
+                  <button
+                    role="button"
+                    disabled=""
+                    className="btn btn-primary text-primary-content disabled:cursor-not-allowed disabled:opacity-60 text-center relative w-full rounded-full"
+                  >
+                    WHATSAPP
+                  </button>
+                  <button
+                    role="button"
+                    disabled=""
+                    className="btn btn-primary text-primary-content disabled:cursor-not-allowed disabled:opacity-60 text-center relative w-full rounded-full"
+                  >
+                    TEXT MESSAGE
+                  </button>
+                </div>
               </div>
             </div>
           </div>
