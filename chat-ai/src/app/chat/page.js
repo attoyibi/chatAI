f79242994c0035a33ed77959d9f00346f6010f5f
sofian hadiwijaya <me.sofian.hadiwijaya@gmail.com>
2023-10-02 21:17:38 +0700

    Sofian update

--- chat-ai/src/app/chat/page.js
+++ chat-ai/src/app/chat/page.js
@@ -2,25 +2,21 @@
 
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { useChat } from 'ai/react';
 import { useRouter } from "next/navigation";
-import { Configuration, OpenAIApi } from "openai";
-import { OpenAI } from "openai";
-// Inisialisasi array conversations
-const initialConversations = [
-  {
-    // sender: "Chat AI",
-    profileImage: "https://cdn-icons-png.flaticon.com/512/4712/4712109.png",
-    message: "Halo! Apa yang bisa saya bantu?",
-  },
-];
+import { headers } from "../../../next.config";
+import cookieCutter from 'cookie-cutter'
 
-function Page() {
+export default function Page() {
   const router = useRouter();
+  const [accessToken, setAccessToken] = useState("");
+
   useEffect(() => {
     // Check if accessToken is present in localStorage
-    const accessToken = localStorage.getItem("accessToken");
+    const accessToken = cookieCutter.get('accessToken')
 
     if (accessToken) {
+      setAccessToken(accessToken);
       // Access token is available, you can perform actions accordingly
       // console.log("Access token found:", accessToken);
       // You can use this accessToken for making authenticated requests, etc.
@@ -32,69 +28,20 @@ function Page() {
     }
   }, []); // The empty dependency array ensures this effect runs once when the component mounts
 
-  //openai api configuration
-  const openai = new OpenAI({
-    apiKey: "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",
-    dangerouslyAllowBrowser: true,
-  });
-
-  // const openai = new OpenAIApi(configuration);
-
-  const [inputText, setInputText] = useState(""); // State untuk input teks
-  const [conversations, setConversations] = useState(initialConversations);
-  const chatOpenAI = async () => {
-    setLoading(true);
-    try {
-      const chatCompletion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: inputText }],
-      });
-      // Tambahkan pesan pengguna ke dalam array conversations
-      setConversations([
-        ...conversations,
-        {
-          // sender: "You",
-          profileImage:
-            "https://cdn-icons-png.flaticon.com/512/4712/4712109.png",
-          message: chatCompletion.choices[0].message.content,
-        },
-      ]);
-      setLoading(false);
-      console.log(chatCompletion.choices[0].message);
-    } catch (error) {
-      console.error(error);
-    }
-
-    console.log(conversations);
-  };
-  useEffect(() => {
-    if (inputText !== "") {
-      chatOpenAI();
-      // Reset input teks
-      setInputText("");
-    }
-  }, [conversations]);
-
-  const [loading, setLoading] = useState(false);
-
-  const handleClick = async () => {
-    if (inputText.trim() !== "") {
-      // Tambahkan pesan pengguna ke dalam array conversations
-      setConversations([
-        ...conversations,
-        {
-          // sender: "You",
-          profileImage:
-            "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg",
-          message: inputText,
-        },
-      ]);
+  
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  
+  const onEnter = (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();  // This will prevent adding a new line
+        handleSubmit(e);
     }
   };
+
   return (
     <div className="bg-white relative max-w-[500px] mx-auto w-full flex flex-col min-h-screen shadow-xl shadow-primary/40">
       <div className="fixed w-full z-10">
-        <div className="grid grid-cols-9 gap-3 px-5 py-3 max-w-[500px] bg-white shadow-md shadow-primary/15">
+        <div className="grid grid-cols-1 gap-4 max-w-[500px] bg-white shadow-md shadow-primary/15">
           <div className="flex items-center">
             <Link href="/">
               <button className="btn-ghost p-2 rounded-xl">
@@ -110,80 +57,49 @@ function Page() {
                 </svg>
               </button>
             </Link>
-          </div>
-          <div className="col-start-3 col-end-8 flex items-center justify-center">
-            {/* <p className="text-xl font-semibold">You</p> */}
+            <div className="flex items-center gap-3">
+                <div className="avatar">
+                    <div className="w-20 rounded-full">
+                        <img src="https://warungpintar.co.id/static/1b6dc3179de91d3f1d89341539f174c1/042ad/wpg-sofian.webp" />
+                    </div>
+                </div>
+                <div>
+                    <p className="font-bold text-lg">Sofian Hadiwijaya</p>
+                    <p>Aktifis</p>
+                </div>
+            </div>
           </div>
         </div>
       </div>
-      {/* chat */}
       <div className="mt-20 mb-60">
-        {conversations.map((conversation, index) => (
-          <div
-            className={`chat ${index % 2 === 0 ? "chat-start" : "chat-end"}`}
-            key={index}
-          >
-            <div className="chat-image avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  src={conversation.profileImage}
-                  alt={`${conversation.sender}'s profile`}
-                />
-              </div>
-            </div>
-            <div className="chat-header">{conversation.sender}</div>
-            <div className="chat-bubble">{conversation.message}</div>
-          </div>
-        ))}
+        {messages.length > 0
+            ? messages.map((m, index) => (
+                <div 
+                key={m.id} 
+                className={`${index % 2 === 0 ? 'chat chat-end' : 'chat chat-start'}`}>
+                    <div className={`${index % 2 === 0 ? 'chat-bubble chat-bubble-primary' : 'chat-bubble chat-bubble-warning'}`}>
+                        {m.content}
+                        </div>
+                </div>
+            ))
+            : null}
       </div>
 
       {/* div bottom */}
       <div className="fixed w-full bottom-0">
         <div className="max-w-[500px] bg-white px-6 py-4 animate-[from-b-25_.35s_ease-in-out]">
-          <div className="flex items-center justify-between">
-            <div className="w-full max-w-xs mr-2">
+          <div className="w-full items-center justify-between">
               <input
                 type="text"
                 placeholder="Write Something"
                 className="input input-bordered w-full"
-                value={inputText}
-                onChange={(e) => setInputText(e.target.value)}
+                value={input}
+                onChange={handleInputChange}
+                onKeyDownCapture={onEnter}
               />
-            </div>
-            {loading ? (
-              <a className="btn btn-primary text-primary-content text-center relative rounded-full w-40">
-                <svg
-                  aria-hidden="true"
-                  role="status"
-                  className="inline w-4 h-4 mr-3 text-white animate-spin"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="#E5E7EB"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentColor"
-                  />
-                </svg>
-                Loading
-              </a>
-            ) : (
-              <a
-                onClick={handleClick}
-                className="btn btn-primary text-primary-content text-center relative rounded-full w-32"
-              >
-                Send
-              </a>
-            )}
           </div>
         </div>
       </div>
     </div>
   );
 }
-
-export default Page;
--- chat-ai/src/app/page.js
+++ chat-ai/src/app/page.js
@@ -3,18 +3,18 @@ export default function Home() {
     <div className="relative max-w-[500px] mx-auto w-full flex flex-col min-h-screen shadow-xl shadow-primary/40">
       {/* <button class="btn btn-accent">Ghost</button> */}
       {/* <Login />*/}
-      <div className="min-h-screen bg-primary">
-        <div className="w-full bg-primary text-primary-content flex flex-col justify-end items-center gap-8 pt-8">
+      <div className="min-h-screen bg-warning">
+        <div className="w-full bg-warning text-primary-content flex flex-col justify-end items-center gap-8 pt-8">
           <img
-            alt="Mira Miracle, Avatara mascot"
+            alt="Sofian Hadiwijaya"
             fetchPriority="high"
             width="350"
             height="350"
             decoding="async"
             data-nimg="1"
             className="animate-[from-l-25_.35s_ease-in-out]"
-            srcSet="https://cdn-icons-png.flaticon.com/512/4712/4712109.png 1x, https://cdn-icons-png.flaticon.com/512/4712/4712109.png 2x"
-            src="https://cdn-icons-png.flaticon.com/512/4712/4712109.png"
+            srcSet="https://warungpintar.co.id/static/1b6dc3179de91d3f1d89341539f174c1/042ad/wpg-sofian.webp 1x, https://warungpintar.co.id/static/1b6dc3179de91d3f1d89341539f174c1/042ad/wpg-sofian.webp 2x"
+            src="https://warungpintar.co.id/static/1b6dc3179de91d3f1d89341539f174c1/042ad/wpg-sofian.webp"
             style={{ color: "transparent" }}
           />
         </div>
@@ -22,14 +22,14 @@ export default function Home() {
           <div className="max-w-[500px] bg-white px-6 py-4 animate-[from-b-25_.35s_ease-in-out]">
             <h1 className="font-bold text-lg sm:text-2xl mb-3">
               Chat denganku setiap saat dengan{" "}
-              <strong className="text-[#FC814A]">AI</strong>
+              <strong className="text-[#FC814A]">Sofian</strong>
             </h1>
             <h4 className="text-zinc-400 text-md sm:text-xl mb-6">
-              Rasakan pengalaman berbincang seolah sedang chatting dengan idola
+              Rasakan pengalaman berbincang seolah sedang chatting dengan Calon Legislatif
               favoritmu
             </h4>
             <a
-              className="btn btn-primary text-primary-content text-center relative w-full rounded-full"
+              className="btn btn-warning text-warning-content text-center relative w-full rounded-full"
               href="/join"
             >
               Chat Now
